df54d043d1374369bfa7860a0462101e3d769d3c
abhirup009 <abhirup.acharya009@gmail.com>
2021-11-09 23:23:47 +0530

    Add logger

--- src/main/kotlin/api/platform/lobby/data/impl/DbUserDao.kt
+++ src/main/kotlin/api/platform/lobby/data/impl/DbUserDao.kt
@@ -5,6 +5,8 @@ import api.platform.lobby.model.ProspectiveUser
 import api.platform.lobby.utils.toProspectiveUser
 import api.platform.lobby.utils.toRecord
 import org.jooq.DSLContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import recommender.jooq.Tables
 
@@ -12,7 +14,12 @@ import recommender.jooq.Tables
 class DbUserDao(
     private val dslContext: DSLContext
 ): UserDao {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(DbUserDao::class.java)
+    }
+
     override fun newUser(prospectiveUser: ProspectiveUser): ProspectiveUser {
+        logger.info("Adding new user: $prospectiveUser to db.")
         return dslContext.insertInto(Tables.PROSPECTIVE_USER)
             .set(prospectiveUser.toRecord())
             .returning()
@@ -21,6 +28,7 @@ class DbUserDao(
     }
 
     override fun getUser(prospectiveUserId: String): ProspectiveUser {
+        logger.info("Getting user details from db.")
         return dslContext.selectFrom(Tables.PROSPECTIVE_USER)
             .where(Tables.PROSPECTIVE_USER.PROSPECTIVE_USER_ID.eq(prospectiveUserId))
             .fetchOne()
